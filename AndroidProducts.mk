24378cef66b8c3f95838aca53fb113d36c7ad662
BasharatKar <basharatkar1@gmail.com>
2021-12-07 15:42:39 +0000

    sweet: xtended bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/xtended_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    xtended_sweet-eng \
+    xtended_sweet-userdebug \
+    xtended_sweet-user
